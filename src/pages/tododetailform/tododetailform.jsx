1a4e31f76a130d5dd8a22536d951515a7334940d
Yashkharel19 <yash048339@gmail.com>
2023-07-11 20:55:17 -0400

    todo detail page done

--- src/pages/tododetailform/tododetailform.jsx
+++ src/pages/tododetailform/tododetailform.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import './tododetailform.css'
 import axios from 'axios';
@@ -20,12 +20,25 @@ function TodoDetails() {
 
   return (
     <div className="todo-details-container">
-
+      <div className="header-container">
+        <h1 className="todo-details-header">Todo Details</h1>
+        <div className="buttons-container">
+          <Link to="/add">
+            <button className="add-todo-button">Add Todo</button>
+          </Link>
+          <Link to="/">
+            <button className="todo-list-button">Todo List</button>
+          </Link>
+        </div>
+      </div>
       <div className="todo-card">
-        <h1>Todo Details</h1>
         <div className="card-content">
-          <h3 className="todo-title">Todo ID: <span className="todo-id">{apiData.id}</span></h3>
-          <h2 className="todo-title">Task Name:<span className="todo-id"> {apiData.title}</span></h2>
+          <h3 className="todo-title">
+            Todo ID: <span className="todo-id">{apiData.id}</span>
+          </h3>
+          <h2 className="todo-title">
+            Task Name: <span className="todo-id">{apiData.title}</span>
+          </h2>
           <div className="status-container">
             <strong className="status-label">Status:</strong>
             {apiData.completed ? (
