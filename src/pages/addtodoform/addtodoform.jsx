796f2b3b91b11b0c0ae699ded277baf06639bae1
Yashkharel19 <yash048339@gmail.com>
2023-07-11 21:05:27 -0400

    Form data UI done

--- src/pages/addtodoform/addtodoform.jsx
+++ src/pages/addtodoform/addtodoform.jsx
@@ -1,44 +1,59 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+import { TextField, Button } from '@mui/material';
+import axios from 'axios';
+import './addtodoform.css'; // Import the CSS file
 
 function AddTodoForm() {
   const [title, setTitle] = useState('');
   const [completed, setCompleted] = useState(false);
+  // const history = useHistory();
 
-  const handleSubmit = event => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
+    const newTodo = {
+      title: title,
+      completed: completed,
+    };
 
-    // Logic to add the new todo to the todo list
-    // ...
-
-    // Reset form fields
-    setTitle('');
-    setCompleted(false);
+    try {
+      await axios.post('https://jsonplaceholder.typicode.com/todos', newTodo);
+      // history.push('/'); // Redirect to the todo list page after adding the todo
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div>
-      <h1>Add Todo</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Title:
-          <input
-            type="text"
+    <div className="todo-add-container">
+      <div className="todo-card">
+        <h1>Add Todo</h1>
+        <form onSubmit={handleFormSubmit} className="todo-form">
+          <TextField
+            label="Title"
+            variant="outlined"
             value={title}
             onChange={event => setTitle(event.target.value)}
+            className="input-field"
+            required
           />
-        </label>
-        <br />
-        <label>
-          Completed:
-          <input
-            type="checkbox"
-            checked={completed}
-            onChange={event => setCompleted(event.target.checked)}
-          />
-        </label>
-        <br />
-        <button type="submit">Add Todo</button>
-      </form>
+          <div className="checkbox-container">
+            <label htmlFor="completed" className="checkbox-label">
+              Completed:
+            </label>
+            <input
+              type="checkbox"
+              id="completed"
+              checked={completed}
+              onChange={event => setCompleted(event.target.checked)}
+              className="checkbox-input"
+            />
+          </div>
+          <Button type="submit" variant="contained" color="primary" className="submit-button">
+            Add
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
